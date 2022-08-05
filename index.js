f8c914493ea74a8589c59eed0d4ca497702a02c3
Hpsein Khansari <hrk.x11@gmail.com>
2022-08-05 13:53:13 +0430

    Fix some of the possible issues
    - silently fail if there's no new photo
    - resolve the absolute url using the URL class

--- index.js
+++ index.js
@@ -11,24 +11,26 @@ const BBF = 'https://breakingbadfunny.com'
 const db = new JSONFile('db.json')
 const bot = new Telegraf(process.env.TOKEN)
 
-async function getImage(index) {
+async function post() {
   const html = await fetch(BBF).then(r => r.text())
   const document = parse(html)
   const comics = document.querySelectorAll('.comic')
+  const postedCount = (await db.read())?.picturesCount ?? 0
+
+  if (postedCount >= comics.length) {
+    // In this case all of the photos have been posted so we don't need to go further
+    return false
+  }
+
   // get the n-th last image
-  const comic = comics[comics.length - index - 1]
-  const image = comic.querySelector('img')
-  const url = `${BBF}/${image.getAttribute('src')}`
-  return url
-}
-
-async function post() {
-  const picturesCount = (await db.read())?.picturesCount ?? 0
-  const url = await getImage(picturesCount)
+  const comic = comics[comics.length - postedCount - 1]
+  const imagePath = comic.querySelector('img').getAttribute('src')
+  const url = new URL(imagePath, BBF).href
   await bot.telegram.sendPhoto(`@${process.env.CHANNEL_ID}`, url)
-  await db.write({ picturesCount: picturesCount + 1 })
+  await db.write({ picturesCount: postedCount + 1 })
+  return true
 }
 
-post().then(() => {
-  console.log('methposting done successfully')
+post().then(posted => {
+  console.log(posted ? 'methposting done successfully' : 'no new post')
 }, console.log)
